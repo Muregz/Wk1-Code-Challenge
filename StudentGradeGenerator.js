e5280946800f12ac91b9ed1a0abfb36b38c5d685
Muregz <fabian.murega@student.moringaschool.com>
2024-07-08 08:45:55 +0300

    master

---
+++ StudentGradeGenerator.js
@@ -0,0 +1,25 @@
+function calculateGrade() {
+    let marks = prompt("Enter student marks (between 0 and 100):");
+
+    marks = parseFloat(marks);
+    
+
+    if (!isNaN(marks) && marks >= 0 && marks <= 100) {
+        let grade;
+     
+        if (marks > 79) {
+            grade = "A";
+        } else if (marks >= 60) {
+            grade = "B";
+        } else if (marks >= 50) {
+            grade = "C";
+        } else if (marks >= 40) {
+            grade = "D";
+        } else {
+            grade = "E";
+        }
+        console.log(`Grade: ${grade}`);
+    } else {
+        console.log("Invalid input. Please enter a number between 0 and 100.");
+    }
+}
